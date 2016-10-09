0890fca9ae458f6dfc43f238ad4e5f479fc71368
Davian Robinson <davianr@gmail.com>
2016-10-09 23:31:59 +0100

    added test project

---
+++ RangeConverterConsoleAppTests/ProgramTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RangeConverterConsoleApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeConverterConsoleApp.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void ConvertRangesToSequenceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ConvertSequenceToRangesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
