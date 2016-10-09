61a0868b00e0836cb6f4d94d53dda4996a1482b9
Davian Robinson <davianr@gmail.com>
2016-10-09 23:48:04 +0100

    updated test

--- RangeConverterConsoleAppTests/ProgramTests.cs
+++ RangeConverterConsoleAppTests/ProgramTests.cs
@@ -14,13 +14,45 @@ namespace RangeConverterConsoleApp.Tests
         [TestMethod()]
         public void ConvertRangesToSequenceTest()
         {
+
+            // Example: sequence(“1”)=[1]
+            // Example: sequence(“1,2”)=[1,2]
+            // Example: sequence(“1:3,5”)=[1,2,3,5]
+
+
             Assert.Fail();
         }
 
         [TestMethod()]
         public void ConvertSequenceToRangesTest()
         {
-            Assert.Fail();
+
+            // Example: ranges(1) = “1”
+            IEnumerable<int> testnumbers = new int[] { 1 };
+            var result = RangeConverterConsoleApp.Program.ConvertSequenceToRanges(testnumbers);
+            Assert.AreEqual("1", result);
+
+        }
+        [TestMethod()]
+        public void ConvertTwoItemSequenceToRangesTest()
+        {
+            // Example: ranges([1,2]) = “1:2”             
+
+            IEnumerable<int> testnumbers = new int[] { 1, 2 };
+            var result = RangeConverterConsoleApp.Program.ConvertSequenceToRanges(testnumbers);
+            Assert.AreEqual("1:2", result);
+
+        }
+
+        [TestMethod()]
+        public void ConvertMultipleItemsSequenceToRangesTest()
+        {
+
+            // Example: ranges([1,2,3,5]) = “1:3,5” 
+            IEnumerable<int> testnumbers = new int[] { 1, 2, 3, 5 };
+            var result = RangeConverterConsoleApp.Program.ConvertSequenceToRanges(testnumbers);
+            Assert.AreEqual("1:3, 5", result);
+
         }
     }
 }
